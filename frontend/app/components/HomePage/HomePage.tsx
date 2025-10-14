a98deea5a4232d18c497f8b14a27f735e3d9df67
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-10-14 06:11:55 +0300

    download icons package, display them on the navigation

--- frontend/app/components/HomePage/HomePage.tsx
+++ frontend/app/components/HomePage/HomePage.tsx
@@ -3,6 +3,10 @@ import Navigation from "../Navigation";
 export default function HomePage() {
     return (
         <View className="w-full h-full">
+            <View className="px-5 pt-6">
+                {/* content here later on */}
+                {/* <Text className="font-bold text-[24px] text-white">Choose your bike</Text> */}
+            </View>
             <Image
                 source={require("../../../assets/images/BG.png")}
                 className="absolute w-[100%] h-full right-0 bottom-[-145px]"
--- frontend/app/components/Navigation.tsx
+++ frontend/app/components/Navigation.tsx
@@ -1,20 +1,43 @@
 import { View, Text } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { BlurView } from "expo-blur";
-
+import { Bike, Map, ShoppingCart, User, FileText } from 'lucide-react-native'
 export default function Navigation() {
+    const navigationOptions = [
+        {
+            icon: <Bike color='white' />
+        },
+        {
+            icon: <Map color='white' />
+        },
+        {
+            icon: <ShoppingCart color='white' />
+        },
+        {
+            icon: <User color='white' />
+        },
+        {
+            icon: <FileText color='white' />
+        },
+    ]
     return (
         <View className="absolute bottom-0 w-full">
             <LinearGradient
                 colors={["#363E51", "#181C24"]}
-                style={{ position: "absolute", top: -13, left: 0, right: 0, bottom: -35, opacity: 0.5 }}
+                style={{ position: "absolute", top: -35, left: 0, right: 0, bottom: -35, opacity: 0.5 }}
             />
             <BlurView
                 intensity={40}
                 tint="dark"
-                style={{ position: "absolute", top: -13, left: 0, right: 0, bottom: -30 }}
+                style={{ position: "absolute", top: -35, left: 0, right: 0, bottom: -30 }}
             />
-            <Text className="text-white text-center p-4">Hey?</Text>
+            <View className="w-full h-full px-12 flex flex-row items-center justify-between">
+                {navigationOptions.map((option, index) => (
+                    <View key={index}>
+                        {option.icon}
+                    </View>
+                ))}
+            </View>
         </View>
     );
 }
