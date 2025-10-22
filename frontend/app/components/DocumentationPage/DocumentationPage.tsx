511d44cf50f6ffd2b640c0c947c70eb8d1944635
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-10-22 06:58:22 +0300

    create other plaholder pages for the navigation

---
+++ frontend/app/components/DocumentationPage/DocumentationPage.tsx
@@ -0,0 +1,8 @@
+import { View, Text } from "react-native";
+export default function DocumentationPage() {
+    return (
+        <View className="flex-1 items-center justify-center bg-[#181C24]">
+            <Text className="text-white text-xl">Documentation Page</Text>
+        </View>
+    );
+}
---
+++ frontend/app/components/ShopPage/ShopPage.tsx
@@ -0,0 +1,8 @@
+import { View, Text } from "react-native";
+export default function ShopPage() {
+    return (
+        <View className="flex-1 items-center justify-center bg-[#181C24]">
+            <Text className="text-white text-xl">Shop Page</Text>
+        </View>
+    );
+}
