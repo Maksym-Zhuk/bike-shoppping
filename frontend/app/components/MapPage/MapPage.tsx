ca7c2f06684e39f72797d4c39a7bff38854d1fd7
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-10-22 06:54:46 +0300

    create map page layout ( for future navigation testing )

---
+++ frontend/app/components/MapPage/MapPage.tsx
@@ -0,0 +1,8 @@
+import { View, Text } from "react-native";
+export default function MapPage() {
+    return (
+        <View className="flex-1 items-center justify-center bg-[#181C24]">
+            <Text className="text-white text-xl">Map Page</Text>
+        </View>
+    );
+}
