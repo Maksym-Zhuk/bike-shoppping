c5a66ceaabb4cbae96f2d2ca676026814a6c85a6
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-10-31 05:13:00 +0200

    create scrolling on the main page

--- frontend/app/components/HomePage/HomePage.tsx
+++ frontend/app/components/HomePage/HomePage.tsx
@@ -1,46 +1,57 @@
 import React from "react";
-import { View, Text, Image, Pressable } from "react-native";
+import { View, Text, Image, Pressable, ScrollView } from "react-native";
 import SearchBar from "./SearchBar";
 import BannerShape from "./BannerShape";
 import AppIcons from "./AppIcons";
 import Products from "./Products";
-import { useRouter } from "expo-router"
+import { useRouter } from "expo-router";
 
 export default function HomePage() {
-  var router = useRouter();
+  const router = useRouter();
+
   return (
-    <View className="flex-1">
+    <View className="flex-1 bg-[#242C38]">
       <Image
         source={require("../../../assets/images/BG.png")}
         className="absolute w-full h-full right-0 bottom-[-145px] z-0"
         resizeMode="cover"
       />
 
-      <View className="px-5 pt-4 z-20">
-        <SearchBar />
-      </View>
+      <ScrollView
+        className="flex-1"
+        contentContainerStyle={{
+          paddingBottom: 60,
+        }}
+        showsVerticalScrollIndicator={false}
+      >
+        <View className="px-5 pt-4 z-20">
+          <SearchBar />
+        </View>
 
-      <BannerShape className="-mt-5">
-        <Pressable onPress={() => {
-          router.push({
-            pathname: "/product",
-            params: {
-              a: "1"
+        <BannerShape className="-mt-5">
+          <Pressable
+            onPress={() =>
+              router.push({
+                pathname: "/product",
+                params: { a: "1" },
+              })
             }
-          });
-        }} className="w-full items-center justify-center relative">
-          <Image
-            source={require("../../../assets/images/banner-bike-sample.png")}
-            className="w-[90%] h-[170px] z-10"
-            resizeMode="contain"
-          />
-          <Text className="absolute text-[rgba(255,255,255,0.7)] text-[32px] font-bold top-[175px] left-[25px]">30% Off</Text>
-        </Pressable>
-      </BannerShape>
+            className="w-full items-center justify-center relative"
+          >
+            <Image
+              source={require("../../../assets/images/banner-bike-sample.png")}
+              className="w-[90%] h-[170px] z-10"
+              resizeMode="contain"
+            />
+            <Text className="absolute text-[rgba(255,255,255,0.7)] text-[32px] font-bold top-[175px] left-[25px]">
+              30% Off
+            </Text>
+          </Pressable>
+        </BannerShape>
 
-      <AppIcons />
-      <Products />
-
+        <AppIcons />
+        <Products />
+      </ScrollView>
     </View>
   );
 }
